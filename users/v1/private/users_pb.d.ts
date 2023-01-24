c6bbe3de63f4e6f97176d2a95e685785b44183c6
keegancraigcruickshank <keegan@cloudpixel.com.au>
2023-01-24 08:35:55 +0000

    [SCHEMA] - Update from schema

--- users/v1/private/users_pb.d.ts
+++ users/v1/private/users_pb.d.ts
@@ -41,14 +41,14 @@ export declare class User extends Message<User> {
   lastLogout: bigint;
 
   /**
-   * @generated from field: string created_at = 7;
+   * @generated from field: int64 created_at = 7;
    */
-  createdAt: string;
+  createdAt: bigint;
 
   /**
-   * @generated from field: string updated_at = 8;
+   * @generated from field: int64 updated_at = 8;
    */
-  updatedAt: string;
+  updatedAt: bigint;
 
   /**
    * @generated from field: string default_org = 9;
